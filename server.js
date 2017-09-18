c116f5daddf7c3204ebf24337f7853fc206b81aa
Bessy Theodoropoulou <mellonance@gmail.com>
2017-09-18 16:12:56 +0300

    Add server setup

--- server.js
+++ server.js
@@ -2,20 +2,22 @@ const express = require('express');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpack = require('webpack');
 const webpackConfig = require('./webpack.config.js');
+const path = require('path');
 const app = express();
  
 const compiler = webpack(webpackConfig);
  
 app.use(express.static(__dirname + '/www'));
  
-app.use(webpackDevMiddleware(compiler, {
+app.use(webpackDevMiddleware(webpack(webpackConfig), {
   hot: true,
-  filename: 'bundle.js',
-  publicPath: '/',
-  stats: {
-    colors: true,
-  },
-  historyApiFallback: true,
+  // filename: 'bundle.js',
+  publicPath: webpackConfig.output.publicPath,
+  contentBase: path.resolve(__dirname, 'www'),
+  // stats: {
+  //   colors: true,
+  // },
+  historyApiFallback: true
 }));
  
 const server = app.listen(3000, function() {
