ee8fe7a499cae813dfd282d0d31198f3f3496ed1
Bessy Theodoropoulou <mellonance@gmail.com>
2017-10-04 04:22:21 +0300

    Add navigation bar component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Home from './Components/Home';
 import About from './Components/About';
 import Header from './Components/Header';
+import NavBar from './Components/NavBar';
 import Reserve from './Components/Reserve';
 import './Styles/variables.scss';
 
@@ -10,6 +11,7 @@ class App extends Component {
   render() {
     return (
       <div className="content">
+        <NavBar />
         <Header />
         <Home />
         <About />
